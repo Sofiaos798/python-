f9bedf9a866167ac0ae1f5d1bb1159fe2a13ea6c
Sofia Ostapenko <ostapenkos2202@gmail.com>
2025-09-17 21:22:36 +0300

    Update завд 3.py

--- завд 3.py
+++ завд 3.py
@@ -1,16 +1,13 @@
 n = 5
-shift = 4  # дополнительный сдвиг вправо для всего верхнего треугольника
-
-# Верхний треугольник с прямым углом слева
+shift = 4  
 for i in range(1, n + 1):
-    print("  " * shift, end="")  # общий сдвиг вправо
-    for j in range(i, 0, -1):    # числа от i до 1
+    print("  " * shift, end="") 
+    for j in range(i, 0, -1):  
         print(j, end=" ")
     print()
-
-# Нижний треугольник (как есть)
 for i in range(n, 0, -1):
     print("  " * (n - i), end="")
     for j in range(1, i + 1):
         print(j, end=" ")
     print()
+
